37b0ec0247f10bd40402d6f4fddbab925e5f0b4a
Sajan Sarker <sajansarker47@gmail.com>
2024-06-20 19:11:28 +0600

    whole project completed

--- project.py
+++ project.py
@@ -1,8 +1,6 @@
 # Description: This file contains the code for the project.
 from fpdf import FPDF
 
-#def create_resume(basic_details, skills, experience, education, projects):
-
 pdf = FPDF()
 pdf = FPDF(orientation="portrait", format="a4")
 pdf.add_page()
@@ -19,11 +17,29 @@ def contact(number, email, linkedin):
     pdf.set_line_width(1)
     pdf.cell(0, 6, number +"                    "+ email +"                    "+ linkedin, 0, 0, "C", True)
     pdf.ln(10)
-
 def sec_header(header):
     pdf.set_font("Arial", "B", 12)
     pdf.cell(0, 6, header, 0, 0, "L")
-    pdf.ln(8)
+    pdf.ln(6)
+def body_header(position, date):
+    pdf.set_font("Arial", "B", 11)
+    pdf.cell(0, 6, position, 0, 0, "L")
+    pdf.set_font("Arial", "", 9)
+    pdf.cell(0, 0, date, 0, 0, "R")
+    pdf.ln(5)
+def body_sub_header(company):
+    pdf.set_font("Arial", "", 11)
+    pdf.cell(0, 6, company, 0, 0, "L")
+    pdf.ln(9)
+def print_body_sub_header(text):
+    pdf.set_font("Arial", "", 10)
+    pdf.cell(0, 0, text, 0, 0, "L")
+    pdf.ln(6)
+def print_bullet_point(details):
+    pdf.set_font("Arial", "", 10)
+    for detail in details:
+        pdf.cell(0, 0, "- "+detail, 0, 0, "L")
+        pdf.ln(6)
 
 def paragraph(description):
     pdf.set_font("Arial", "", 10)
@@ -32,48 +48,41 @@ def paragraph(description):
     else:
         description = ", ".join(description)+"."
         pdf.multi_cell(0, 5, description)
-
-
     pdf.ln(8)
 
-def create_resume(basic_details, skills):
+#def create_resume(basic_details, skills, experience, education, projects):
+def create_resume(basic_details, skills, experiences, education, projects):
     header(basic_details["name"])
     sub_header(basic_details["address"])
     contact(basic_details["phone_number"], basic_details["email"], basic_details["linkedin"])
+
     sec_header("Description:")
     paragraph(basic_details["description"])
+
     sec_header("Skills: ")
     paragraph(skills)
 
+    sec_header("Experiences: ")
+    for job in experiences:
+        body_header(job["position"], "Year: "+job["date"])
+        body_sub_header(job["company"])
+        print_bullet_point(job["details"])
+
+    sec_header("Education: ")
+    for edu in education:
+        body_header(edu["university"], "Year: "+edu["year"])
+        body_sub_header(edu["degree"])
+        print_body_sub_header("CGPA: "+edu["cgpa"])
+    project = [{"name": "Eco Mart", "type": "Web app", "about": ["Used HTML, CSS, PHP, JavaScript, MySQL.",
+                                                                 "It have admin panal, and employee panel, and basic user panel."]}]
+
+    sec_header("Projects: ")
+    for project in projects:
+        body_header(project["name"], "")
+        body_sub_header(project["type"])
+        print_bullet_point(project["about"])
+
     pdf.output("Resume.pdf")
-
-
-
-
-    #pdf.set_font("Arial","B", size=13)
-    #pdf.cell(200, 10, basic_details[0], ln=True, align="C")
-    """
-    pdf.set_font("Arial", style="B", size=16)
-    pdf.cell(200, 10, "Resume", ln=True, align="C")
-
-    create_section("Basic Details", "")
-    create_line()
-    create_box("Name: " + basic_details["name"])
-    create_box("Phone Number: " + basic_details["phone_number"])
-    create_box("Email: " + basic_details["email"])
-    create_box("Address: " + basic_details["address"])
-    create_box("Motivation: " + basic_details["motivation"])
-    """
-
-
-def create_box(text):
-    ...
-
-def create_section(title, content):
-    ...
-
-def create_line():
-    ...
 
 def main():
     """
@@ -81,7 +90,7 @@ def main():
     phone_number = input("Number: ")
     email = input("Email: ")
     address = input("Present Address: ")
-    motivation = input("Motivation: ")
+    description = input("Description: ")
     basic_details = {"name": name, "phone_number": phone_number, "email": email, "address": address, "motivation": motivation}
 
     skills = []
@@ -123,7 +132,6 @@ def main():
         project_description = input("Description: ")
         projects.append({"project": project, "link": link, "project_description": project_description})
     """
-    #create_resume(basic_details, skills, experience, education, projects)
     basic_details = {
         "name": "John Doe",
         "phone_number": "123-456-7890",
@@ -133,9 +141,22 @@ def main():
         "linkedin": "https://www.linkedin.com/in/john-doe"
     }
     skills = ['Algorithms', 'Data Structures', 'Programming Languages (e.g., Python, C++, Java)', 'Software Development', 'Database Management', 'Operating Systems', 'Network Security', 'Machine Learning', 'Web Development', 'Cloud Computing']
-    experience =
+    experience = [{"position": "Software Engineer", "date": "09/2015 to 05/2019", "company": "Luna Software, New York",
+                   'details': [
+                       "Investigation, design, and implement scalable applications for data identification, analysis, retrieval, and indexing.",
+                       "Software design and development while remaining concentrate on client needs.",
+                       "Estimate interface between hardware and software."]},
+                  {"position": "Junior Software Engineer", "date": "09/2014 to 05/2015",
+                   "company": "AdsPro Software, New York", 'details': [
+                      "Consulted regularly with customers on project status, proposals and technical issues.",
+                      "Transformed existing software to correct errors, upgrade interfaces, and improve efficiency."]}]
 
-    create_resume(basic_details, skills)
+    education = [{"degree": "Computer Science & Engineering", "university": "North South University", "year": "2024",
+                  "cgpa": "3.99"}]
+    project = [{"name": "Eco Mart", "type": "Web app", "about": ["Used HTML, CSS, PHP, JavaScript, MySQL.",
+                                                                 "It have admin panal, and employee panel, and basic user panel."]}]
+
+    create_resume(basic_details, skills, experience, education, project)
 
     return 0
 
