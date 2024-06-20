baf01dd367dce1cd86f8f0ae5bed84394a242e00
Sajan Sarker <sajansarker47@gmail.com>
2024-06-20 17:21:12 +0600

    implemented header, sub_header, contact, sec_header, paragraph functions

--- project.py
+++ project.py
@@ -7,16 +7,47 @@ pdf = FPDF()
 pdf = FPDF(orientation="portrait", format="a4")
 pdf.add_page()
 def header(name):
-    pdf.set_font("Arial", "B", 16)
+    pdf.set_font("Arial", "B", 22)
     pdf.cell(0, 10, name, 0, 1, "C")
-def sub_header(title):
+def sub_header(address):
     pdf.set_font("Arial", "", 10)
-    pdf.cell(0, 10, title, 0, 0, "C")
+    pdf.cell(0, 6, address, 0, 0, "C")
+    pdf.ln(8)
+def contact(number, email, linkedin):
+    pdf.set_font("Arial", "", 10)
+    pdf.set_fill_color(200,220,255)
+    pdf.set_line_width(1)
+    pdf.cell(0, 6, number +"                    "+ email +"                    "+ linkedin, 0, 0, "C", True)
+    pdf.ln(10)
 
-def create_resume(basic_details):
+def sec_header(header):
+    pdf.set_font("Arial", "B", 12)
+    pdf.cell(0, 6, header, 0, 0, "L")
+    pdf.ln(8)
+
+def paragraph(description):
+    pdf.set_font("Arial", "", 10)
+    if type(description) == str:
+        pdf.multi_cell(0, 5, description)
+    else:
+        description = ", ".join(description)+"."
+        pdf.multi_cell(0, 5, description)
+
+
+    pdf.ln(8)
+
+def create_resume(basic_details, skills):
     header(basic_details["name"])
-    sub_header(basic_details["position"])
+    sub_header(basic_details["address"])
+    contact(basic_details["phone_number"], basic_details["email"], basic_details["linkedin"])
+    sec_header("Description:")
+    paragraph(basic_details["description"])
+    sec_header("Skills: ")
+    paragraph(skills)
+
     pdf.output("Resume.pdf")
+
+
 
 
     #pdf.set_font("Arial","B", size=13)
@@ -95,13 +126,16 @@ def main():
     #create_resume(basic_details, skills, experience, education, projects)
     basic_details = {
         "name": "John Doe",
-        "position": "Python Developer",
         "phone_number": "123-456-7890",
         "email": "john.doe@example.com",
         "address": "123 Main St, Anytown, USA",
-        "motivation": "I am passionate about... etc."
+        "description": "A Computer Science and Engineering (CSE) engineer is a professional who applies the principles of computer science and electrical engineering to design, develop, and maintain computer systems and software. They are skilled in programming languages, software development, algorithms, data structures, and hardware architecture. CSE engineers work on a wide range of technologies, including operating systems, databases, networks, and cybersecurity. They play a crucial role in advancing technology by creating innovative solutions for complex problems in various industries.",
+        "linkedin": "https://www.linkedin.com/in/john-doe"
     }
-    create_resume(basic_details)
+    skills = ['Algorithms', 'Data Structures', 'Programming Languages (e.g., Python, C++, Java)', 'Software Development', 'Database Management', 'Operating Systems', 'Network Security', 'Machine Learning', 'Web Development', 'Cloud Computing']
+    experience =
+
+    create_resume(basic_details, skills)
 
     return 0
 
